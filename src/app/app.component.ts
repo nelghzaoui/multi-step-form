e26cc39d997249a8dc320b11789e44e2df21deb0
Nasreddine <eln@outlook.be>
2025-03-19 15:09:01 +0100

    update the layout of the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,29 +17,33 @@ import { PersonalInfoComponent } from './components/personal-info.component';
     NextButtonComponent
   ],
   template: `
-    <picture class="absolute inset-0 -z-10">
-      <source media="(min-width: 800px)" srcset="bg-sidebar-mobile.svg" />
-      <source media="(min-width: 480px)" srcset="bg-sidebar-desktop.svg" />
-      <img
-        src="assets/images/bg-sidebar-mobile.svg"
-        alt="Description of the image"
-        class="w-full -z-10"
-      />
-    </picture>
+    <main class="relative min-h-screen bg-magnolia">
+      <picture class="absolute inset-0 z-1">
+        <source media="(min-width: 800px)" srcset="bg-sidebar-mobile.svg" />
+        <source media="(min-width: 480px)" srcset="bg-sidebar-desktop.svg" />
+        <img
+          src="assets/images/bg-sidebar-mobile.svg"
+          alt="Description of the image"
+          class="w-full object-contain"
+        />
+      </picture>
 
-    <div class="flex flex-col items-center p-6 gap-6">
-      <!-- Stepper -->
-      <nas-stepper [currentStep]="currentStep" [maxStep]="4" />
-      <!-- CurrentStep -->
-      <nas-personal-info />
-      <!-- <router-outlet /> -->
+      <!-- Contenu principal positionné au-dessus grâce à relative et z-index -->
+      <div class="relative z-10 flex flex-col justify-between min-h-screen">
+        <div class="flex flex-col items-center p-6 gap-11">
+          <!-- Stepper -->
+          <nas-stepper [currentStep]="currentStep" [maxStep]="4" />
+          <!-- CurrentStep -->
+          <nas-personal-info />
+        </div>
 
-      <!-- Navigation -->
-      <nav class="flex justify-between">
-        <nas-back-button />
-        <nas-next-button />
-      </nav>
-    </div>
+        <!-- Navigation -->
+        <nav class="bg-white p-5 flex justify-between shadow-2xl">
+          <nas-back-button />
+          <nas-next-button />
+        </nav>
+      </div>
+    </main>
   `
 })
 export class AppComponent {
