f20cb7b978d2f87691be611adc1546c00fc7d243
Nasreddine <eln@outlook.be>
2025-03-25 13:42:34 +0100

    add multiple content projection to the card

--- src/app/shared/components/card.component.ts
+++ src/app/shared/components/card.component.ts
@@ -5,15 +5,23 @@ import { Component } from '@angular/core';
   template: `
     <div
       class="bg-white border border-white shadow-xl p-5 py-8 rounded-xl
-               lg:flex lg:py-4 lg:px-4"
+               lg:flex lg:py-4 lg:px-4 lg:relative"
     >
       <img
         src="assets/images/bg-sidebar-desktop.svg"
         alt="Description of the image"
         class="w-full object-contain hidden lg:flex lg:w-auto"
       />
-      <div class="lg:px-[7rem]">
-        <ng-content />
+
+      <div class="absolute top-12 left-12">
+        <ng-content select="card-step"></ng-content>
+      </div>
+
+      <div
+        class="lg:px-[7rem] lg:flex lg:flex-col lg:justify-between lg:pt-10 lg:pb-4"
+      >
+        <ng-content select="card-content" />
+        <ng-content select="card-nav" />
       </div>
     </div>
   `
